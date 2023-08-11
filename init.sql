00985101972071b92ee855ba8de3857b4d5320e1
admin <admin@admindeMac-mini.local>
2023-08-11 14:00:35 +0800

    feat

---
+++ init.sql
@@ -0,0 +1,14 @@
+create database test_db;
+
+use test_db;
+
+DROP TABLE IF EXISTS test_db.users;
+
+CREATE TABLE users (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    address VARCHAR(255),
+    status VARCHAR(50),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
