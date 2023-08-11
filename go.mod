6e261bdec2c8df6fed9b66333ef5dad231d0e183
admin <admin@admindeMac-mini.local>
2023-08-11 15:40:14 +0800

    feat

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module test
 go 1.20
 
 require (
-	github.com/brianvoe/gofakeit/v6 v6.23.1
+	github.com/brianvoe/gofakeit v3.18.0+incompatible
 	github.com/go-sql-driver/mysql v1.7.1
 )
--- main.go
+++ main.go
@@ -4,14 +4,17 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
-	"strings"
-	"sync"
 
-	"github.com/brianvoe/gofakeit/v6"
+	"github.com/brianvoe/gofakeit"
 	_ "github.com/go-sql-driver/mysql"
 )
 
-// generate 100W fake data in mysql
+// 批次插入的大小
+var batchSize = 1000
+
+// 要插入的記錄數量
+var numRecords = 10000 * 100
+
 func main() {
 	// 資料庫連線設定
 	db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/test_db")
@@ -19,54 +22,34 @@ func main() {
 		log.Fatal(err)
 	}
 	defer db.Close()
-	process(2, db)
+	process(db)
 }
 
 // count 為要執行幾次
-func process(count int, db *sql.DB) {
-	var wg sync.WaitGroup
-	for c := 0; c < count; c++ {
-		wg.Add(1)
-		go func() {
-			// 批次插入的大小
-			batchSize := 1000
-			// 要插入的記錄數量
-			numRecords := 10000 * 50
-			// numRecords := 1500
+func process(db *sql.DB) {
 
-			// 建立一個批次切片
-			var batchValues []interface{}
+	queryString := ""
+	for i := 0; i < numRecords; i++ {
+		name := gofakeit.Name()
+		address := gofakeit.Address().Address
+		status := fmt.Sprintf("%d", i%10) // 將數字轉為字串
 
-			for i := 0; i < numRecords; i++ {
-				name := gofakeit.Name()
-				address := gofakeit.Address().Address
-				status := fmt.Sprintf("%d", i%10) // 將數字轉為字串
+		if i == (numRecords - 1) {
+			queryString += fmt.Sprintf("('%s', '%s', '%s')", name, address, status)
+			break
+		}
+		queryString += fmt.Sprintf("('%s', '%s', '%s'), ", name, address, status)
 
-				// 將資料添加到批次切片
-				batchValues = append(batchValues, name, address, status)
-
-				// 每當達到批次大小，執行批次插入
-				if len(batchValues) == batchSize || i == numRecords-1 {
-					sql := "INSERT INTO users (name, address, status) VALUES "
-					valueStrings := make([]string, 0, len(batchValues)/3)
-					for j := 0; j < len(batchValues); j += 3 {
-						valueStrings = append(valueStrings, fmt.Sprintf("('%s', '%s', '%s')", name, address, status))
-					}
-					sql += strings.Join(valueStrings, ", ")
-
-					_, err := db.Exec(sql)
-					if err != nil {
-						log.Fatal(err)
-					}
-					// fmt.Println(sql)
-					// 清空批次切片
-					batchValues = nil
-				}
+		if i != 0 && (i+1)%batchSize == 0 {
+			queryString = queryString[0 : len(queryString)-2]
+			var sql = "INSERT INTO users (name, address, status) VALUES " + queryString
+			_, err := db.Exec(sql)
+			if err != nil {
+				log.Fatal(err)
 			}
-
-			fmt.Printf("插入 %d 筆資料完成\n", numRecords)
-			wg.Done()
-		}()
+			// fmt.Println(sql)
+			queryString = ""
+		}
 	}
-	wg.Wait()
+	fmt.Printf("插入 %d 筆資料完成\n", numRecords)
 }
