425adc097bf718fb5bcb971fce59aed76c2c26a0
admin <admin@admindeMac-mini.local>
2023-08-11 15:04:53 +0800

    feat

--- main.go
+++ main.go
@@ -4,8 +4,8 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
-	"math/rand"
 	"strings"
+	"sync"
 
 	"github.com/brianvoe/gofakeit/v6"
 	_ "github.com/go-sql-driver/mysql"
@@ -19,44 +19,54 @@ func main() {
 		log.Fatal(err)
 	}
 	defer db.Close()
+	process(2, db)
+}
 
-	// 批次插入的大小
-	batchSize := 30
-	// 要插入的記錄數量
-	numRecords := 1000000 * 1
+// count 為要執行幾次
+func process(count int, db *sql.DB) {
+	var wg sync.WaitGroup
+	for c := 0; c < count; c++ {
+		wg.Add(1)
+		go func() {
+			// 批次插入的大小
+			batchSize := 1000
+			// 要插入的記錄數量
+			numRecords := 10000 * 50
+			// numRecords := 1500
 
-	// 建立一個批次切片
-	var batchValues []interface{}
+			// 建立一個批次切片
+			var batchValues []interface{}
 
-	for i := 0; i < numRecords; i++ {
-		name := gofakeit.Name()
-		address := gofakeit.Address().Address
-		status := fmt.Sprintf("%d", i%10) // 將數字轉為字串
-		if rand.Intn(2) == 0 {
-			status = "Inactive"
-		}
+			for i := 0; i < numRecords; i++ {
+				name := gofakeit.Name()
+				address := gofakeit.Address().Address
+				status := fmt.Sprintf("%d", i%10) // 將數字轉為字串
 
-		// 將資料添加到批次切片
-		batchValues = append(batchValues, name, address, status)
+				// 將資料添加到批次切片
+				batchValues = append(batchValues, name, address, status)
 
-		// 每當達到批次大小，執行批次插入
-		if len(batchValues) == batchSize || i == numRecords-1 {
-			sql := "INSERT INTO users (name, address, status) VALUES "
-			valueStrings := make([]string, 0, len(batchValues)/3)
-			for j := 0; j < len(batchValues); j += 3 {
-				valueStrings = append(valueStrings, "(?, ?, ?)")
+				// 每當達到批次大小，執行批次插入
+				if len(batchValues) == batchSize || i == numRecords-1 {
+					sql := "INSERT INTO users (name, address, status) VALUES "
+					valueStrings := make([]string, 0, len(batchValues)/3)
+					for j := 0; j < len(batchValues); j += 3 {
+						valueStrings = append(valueStrings, fmt.Sprintf("('%s', '%s', '%s')", name, address, status))
+					}
+					sql += strings.Join(valueStrings, ", ")
+
+					_, err := db.Exec(sql)
+					if err != nil {
+						log.Fatal(err)
+					}
+					// fmt.Println(sql)
+					// 清空批次切片
+					batchValues = nil
+				}
 			}
-			sql += strings.Join(valueStrings, ", ")
 
-			_, err := db.Exec(sql, batchValues...)
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			// 清空批次切片
-			batchValues = nil
-		}
+			fmt.Printf("插入 %d 筆資料完成\n", numRecords)
+			wg.Done()
+		}()
 	}
-
-	fmt.Printf("插入 %d 筆資料完成\n", numRecords)
+	wg.Wait()
 }
